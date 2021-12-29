19e47196cd6689efd78be3ea67d168560d5be46c
syzymon <s.tworkowski@student.uw.edu.pl>
2021-12-29 11:08:16 +0100

    xavier init

--- pytorch/train.py
+++ pytorch/train.py
@@ -367,7 +367,8 @@ def init_weight(weight, args):
     if args.init == 'uniform':
         nn.init.uniform_(weight, -args.init_range, args.init_range)
     elif args.init == 'normal':
-        nn.init.normal_(weight, 0.0, args.init_std)
+        # nn.init.normal_(weight, 0.0, args.init_std)
+        nn.init.xavier_uniform_(weight)
 
 
 def init_bias(bias):
