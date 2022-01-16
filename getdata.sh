2ca4e0de339679980e42a952462d4be874fcdb46
Piotr Nawrot <pn406263@students.mimuw.edu.pl>
2022-01-17 00:38:32 +0100

    update getdata, we can allow for downloading datasets

--- getdata.sh
+++ getdata.sh
@@ -56,24 +56,24 @@ if [[ ! -d 'wikitext-103' ]]; then
     cd ..
 fi
 
-#echo "- Downloading enwik8 (Character)"
-#if [[ ! -d 'enwik8' ]]; then
-#    mkdir -p enwik8
-#    cd enwik8
-#    wget --continue http://mattmahoney.net/dc/enwik8.zip
-#    wget https://raw.githubusercontent.com/salesforce/awd-lstm-lm/master/data/enwik8/prep_enwik8.py
-#    python3 prep_enwik8.py
-#    cd ..
-#fi
+echo "- Downloading enwik8 (Character)"
+if [[ ! -d 'enwik8' ]]; then
+   mkdir -p enwik8
+   cd enwik8
+   wget --continue http://mattmahoney.net/dc/enwik8.zip
+   wget https://raw.githubusercontent.com/salesforce/awd-lstm-lm/master/data/enwik8/prep_enwik8.py
+   python3 prep_enwik8.py
+   cd ..
+fi
 
-#echo "- Downloading text8 (Character)"
-#if [[ ! -d 'text8' ]]; then
-#    mkdir -p text8
-#    cd text8
-#    wget --continue http://mattmahoney.net/dc/text8.zip
-#    python ../../prep_text8.py
-#    cd ..
-#fi
+echo "- Downloading text8 (Character)"
+if [[ ! -d 'text8' ]]; then
+   mkdir -p text8
+   cd text8
+   wget --continue http://mattmahoney.net/dc/text8.zip
+   python ../../prep_text8.py
+   cd ..
+fi
 #
 #echo "- Downloading Penn Treebank (PTB)"
 #if [[ ! -d 'penn' ]]; then
