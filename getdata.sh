20aaa629c2efab3f5cdc371ba8079c06c6eb8442
Piotr Nawrot <pn406263@students.mimuw.edu.pl>
2022-01-17 01:47:18 +0100

    x

--- getdata.sh
+++ getdata.sh
@@ -71,7 +71,7 @@ if [[ ! -d 'text8' ]]; then
    mkdir -p text8
    cd text8
    wget --continue http://mattmahoney.net/dc/text8.zip
-   python ../../prep_text8.py
+   python ../prep_text8.py
    cd ..
 fi
 #
