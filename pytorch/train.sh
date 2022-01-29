4ca30d46a3bcd50203aeab5c88a7f2459e2c4484
Piotr Nawrot <pn406263@students.mimuw.edu.pl>
2022-01-29 17:04:50 +0100

    2x training on the same node

--- pytorch/train.sh
+++ pytorch/train.sh
@@ -16,5 +16,8 @@
 
 export OMP_NUM_THREADS=1
 
+echo 'Finding free port'
+PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+
 echo 'Run training...'
-python -m torch.distributed.launch --nproc_per_node="$2" train.py --config_file "$1"
+python -m torch.distributed.launch --master_port=$PORT --nproc_per_node="$2" train.py --config_file "$1"
