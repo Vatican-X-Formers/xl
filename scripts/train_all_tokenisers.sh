18d25b1e1a3a171cbc6aec83339a707e0ed0e36a
Piotr Nawrot <pn406263@students.mimuw.edu.pl>
2022-03-12 17:50:44 +0100

    moved scripts to a new directory

---
+++ scripts/train_all_tokenisers.sh
@@ -0,0 +1,12 @@
+for TOKENIZER in unigram wordpiece bpe do
+		for VOCAB_SIZE in 5000 50000 do
+				echo "Running $TOKENIZER $VOCAB_SIZE"
+				python train_tokenizer.py \
+						--corpus_dir /home/pnawrot/piotrek/datasets/text8 \
+						--corpus_split text8 \
+						--save_dir /home/pnawrot/piotrek/datasets/tokenizer/ \
+						--tokenizer_type $TOKENIZER \
+						--vocab_size $VOCAB_SIZE \
+						--dropout 0.0 &
+		done
+done
