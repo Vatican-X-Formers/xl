a51d34919fdfa5cca9e0a24810082651eef48539
Piotr Nawrot <pn406263@students.mimuw.edu.pl>
2022-03-12 17:52:24 +0100

    added dropout training

--- scripts/train_all_tokenisers.sh
+++ scripts/train_all_tokenisers.sh
@@ -10,3 +10,12 @@ for TOKENIZER in unigram wordpiece bpe do
 						--dropout 0.0 &
 		done
 done
+
+echo "Running dropout training"
+python train_tokenizer.py \
+		--corpus_dir /home/pnawrot/piotrek/datasets/text8 \
+		--corpus_split text8 \
+		--save_dir /home/pnawrot/piotrek/datasets/tokenizer/ \
+		--tokenizer_type bpe \
+		--vocab_size 5000 \
+		--dropout 0.1 &
