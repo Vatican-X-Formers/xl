e938c1c8fc09f68d3cfc8fccd61567effdaed568
Piotr Nawrot <pn406263@students.mimuw.edu.pl>
2022-05-15 05:37:13 +0200

    prep of wiki40b

---
+++ download_wiki40b.py
@@ -0,0 +1,27 @@
+from datasets import load_dataset
+from collections import Counter
+import unidecode
+import pdb
+import re
+import os
+
+markers = ['\n_START_ARTICLE_\n', '\n_START_SECTION_\n', '\n_START_PARAGRAPH_\n', '_NEWLINE_']
+specials = ['\u008A', '\u008B', '\u008C', '\u008D']
+unk = '\u008E'
+
+mapping = {}
+base_path = 'data/wiki40b/'
+
+for language in ['pl']:
+    for split in ['train', 'validation', 'test']:
+        dataset = load_dataset('wiki40b', language, split=split, beam_runner='DirectRunner')
+
+        assert unk not in dataset['text']
+        text = unk.join(dataset['text'])
+
+        os.makedirs(os.path.dirname(f'{base_path}{language}'), exist_ok=True)
+
+        filename = f'{base_path}{language}/{split}.raw.txt'
+
+        with open(filename, 'w+') as file:
+            file.write(text)
