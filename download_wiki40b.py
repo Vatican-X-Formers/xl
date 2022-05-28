7c1f581f947a0c5e6ad98a123067af6dfe6b3a37
Piotr Nawrot <p.nawrot99@gmail.com>
2022-05-28 15:29:45 +0200

    download wiki final script

--- download_wiki40b.py
+++ download_wiki40b.py
@@ -1,16 +1,7 @@
 from datasets import load_dataset
-from collections import Counter
-import unidecode
-import pdb
-import re
 import os
 import sys
 
-markers = ['\n_START_ARTICLE_\n', '\n_START_SECTION_\n', '\n_START_PARAGRAPH_\n', '_NEWLINE_']
-specials = ['\u008A', '\u008B', '\u008C', '\u008D']
-unk = '\u008E'
-
-mapping = {}
 base_path = 'data/wiki40b/'
 
 language = sys.argv[1]
@@ -22,8 +13,7 @@ os.chmod(f'data/wiki40b/{language}', 0o777)
 for split in ['train', 'validation', 'test']:
     dataset = load_dataset('wiki40b', language, split=split, beam_runner='DirectRunner')
 
-    assert unk not in dataset['text']
-    text = unk.join(dataset['text'])
+    text = '\n'.join(dataset['text'])
 
     os.makedirs(os.path.dirname(f'{base_path}{language}'), exist_ok=True)
 
